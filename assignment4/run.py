bce901e7f8a5e54f06954b6b446a46adac8956cc
soumyagupta5643 <121498784+soumyagupta5643@users.noreply.github.com>
2024-03-30 18:19:31 +0530

    Update run.py

--- assignment4/run.py
+++ assignment4/run.py
@@ -35,67 +35,63 @@ def main():
 
   # Initialize the corresponding driver of SU2, this includes solver preprocessing
   try:
-      print("Initializing SU2Driver...")
       SU2Driver = pysu2.CSinglezoneDriver(options.filename, options.nZone, comm);
-      print("SU2Driver initialized successfully.")
-      
   except TypeError as exception:
-      print('A TypeError occured in pysu2.CDriver : ',exception)
-    
-      if options.with_MPI == True:
-        print('ERROR : You are trying to initialize MPI with a serial build of the wrapper. Please, remove the --parallel option that is incompatible with a serial build.')
-      else:
-        print('ERROR : You are trying to launch a computation without initializing MPI but the wrapper has been built in parallel. Please add the --parallel option in order to initialize MPI for the wrapper.')
-      return
+    print('A TypeError occured in pysu2.CDriver : ',exception)
+    if options.with_MPI == True:
+      print('ERROR : You are trying to initialize MPI with a serial build of the wrapper. Please, remove the --parallel option that is incompatible with a serial build.')
+    else:
+      print('ERROR : You are trying to launch a computation without initializing MPI but the wrapper has been built in parallel. Please add the --parallel option in order to initialize MPI for the wrapper.')
+    return
+
 
   CHTMarkerID = None
   CHTMarker = 'plate'       # Specified by the user
 
-    # Get all the tags with the CHT option
+  # Get all the tags with the CHT option
   CHTMarkerList =  SU2Driver.GetCHTMarkerTags()
 
-    # Get all the markers defined on this rank and their associated indices.
+  # Get all the markers defined on this rank and their associated indices.
   allMarkerIDs = SU2Driver.GetMarkerIndices()
 
-    # Check if the specified marker has a CHT option and if it exists on this rank.
+  #Check if the specified marker has a CHT option and if it exists on this rank.
   if CHTMarker in CHTMarkerList and CHTMarker in allMarkerIDs.keys():
-      CHTMarkerID = allMarkerIDs[CHTMarker]
+    CHTMarkerID = allMarkerIDs[CHTMarker]
 
-    # Number of vertices on the specified marker (per rank)
-  nVertex_CHTMarker = 0   
-  
-  def __init__(self):
-        # Initialize any necessary variables or parameter
+  # Number of vertices on the specified marker (per rank)
+  nVertex_CHTMarker = 0         # total number of vertices (physical + halo)
 
-
-      if CHTMarkerID != None:
-        nVertex_CHTMarker = SU2Driver.GetNumberMarkerNodes(CHTMarkerID)
-        
-      if nVertex_CHTMarker > 0:
-        marker_coords = SU2Driver.MarkerCoordinates(CHTMarkerID) 
-        
-        
-      #while (x<1.2 and y<7):
-        
-      for i_vertex in range(nVertex_CHTMarker):
-        x = marker_coords(i_vertex, 0) 
-        y = marker_coords(i_vertex, 1)
-        temperature = 293+x+y
-        SU2Driver.SetMarkerCustomTemperature(CHTMarkerID, i_vertex, temperature)
-             
-      SU2Driver.BoundaryConditionsUpdate()
-      SU2Driver.Run()
-      SU2Driver.Postprocess()
-           
-      SU2Driver.Update()
-     # stopCalc = SU2Driver.Monitor(TimeIter)
-     # SU2Driver.Output(TimeIter)
-     # if (stopCalc == True):
-     #    break
-              
+  if CHTMarkerID != None:
+    nVertex_CHTMarker = SU2Driver.GetNumberMarkerNodes(CHTMarkerID)
+  if nVertex_CHTMarker > 0:
+    marker_coords = SU2Driver.MarkerCoordinates(CHTMarkerID) 
  
-      if rank == 0:
-        print("\n------------------------------ End Solver -----------------------------\n")
+
+  # Time loop is defined in Python so that we have acces to SU2 functionalities at each time step
+  if rank == 0:
+    print("\n------------------------------ Begin Solver -----------------------------\n")
+  sys.stdout.flush()
+  if options.with_MPI == True:
+    comm.Barrier()
+  x=0
+  y=0
+  while (x < 1 and y < 4):
+    # Time iteration preprocessing
+    for iVertex in range(nVertex_CHTMarker):
+       x = marker_coords(iVertex, 0) 
+       y = marker_coords(iVertex, 1)
+       temperature = 293+x+y
+       SU2Driver.SetMarkerCustomTemperature(CHTMarkerID, iVertex, temperature)
+
+    # Tell the SU2 drive to update the boundary conditions
+    SU2Driver.BoundaryConditionsUpdate()
+    # Run one time iteration (e.g. dual-time)
+    SU2Driver.Run()
+    # Postprocess the solver and exit cleanly
+    SU2Driver.Postprocess()
+   
+    
+
 
 # -------------------------------------------------------------------
 #  Run Main Program
